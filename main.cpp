9deda5bf6864b4f06e17cbae2f357c7b3f4ce629
Shivaprasad <shivaprasadsmys@gmail.com>
2020-09-10 13:08:08 +0530

    added catch.hpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-
-int main()
-{
-    test_class1();
-    test_class2();
-    return 0;
-}
--- test.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "class1.h"
-#include "class2.h"
-
-void test_class1();
-void test_class2();
