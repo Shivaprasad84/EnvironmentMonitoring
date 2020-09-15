f020923ea2ee133367e97987e1f3f6ed99937655
Shivaprasad <shivaprasadsmys@gmail.com>
2020-09-16 02:23:37 +0530

    changes

--- SenderModule/CSVReader.cpp
+++ SenderModule/CSVReader.cpp
@@ -5,7 +5,7 @@
 
 CSVReader::CSVReader(const std::string& filepath):filepath(filepath) 
 {
-    readFromFile.exceptions( std::ifstream::failbit );
+    readFromFile.exceptions(std::ifstream::badbit);
     try
     {
         readFromFile.open(filepath, std::ios::in);
